c7f84bf428c746ff534380ee0710a1f264f184bb
Nicholas Chan <74500036+n-chan@users.noreply.github.com>
2020-12-03 10:15:20 -0800

    Added a new npc. Fixed position of sparks animation.

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NPC : Interactable {
+    public Dialogue dialogue;
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange) {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue, false);
+        }
+    }
+}
